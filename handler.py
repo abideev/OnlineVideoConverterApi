b799ca826e046209752c43e8e320e2c6219abd2b
abideev <alexander.bideev@freemake.com>
2020-10-11 19:16:43 +0300

    add videoinfo funcation

--- handler.py
+++ handler.py
@@ -1,6 +1,7 @@
 import json
 import pika
 import time
+from videocheckformat import YoutubeParce
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.8.12', port=5672, credentials=pika.credentials.PlainCredentials('rabbitmq', 'rabbitmq'),))
@@ -9,16 +10,32 @@ channel.queue_declare(queue='online_converter_queue', durable=True)
 
 print('-----Wait-----')
 
+def RabbitMQSent(videoinfo,queue_info):
+  connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.8.12', port=5672, credentials=pika.credentials.PlainCredentials('rabbitmq', 'rabbitmq'),))
+  channel = connection.channel()
+  channel.queue_declare(queue=queue_info, durable=True)
+  channel.basic_publish(
+      exchange='',
+      routing_key=queue_info,
+      body=json.dumps(videoinfo),
+      properties=pika.BasicProperties(
+          delivery_mode=2,
+      ))
+  connection.close()
+
+
+def ReturnVideoInfo(url):
+  queue_info = "video_info_queue"
+  videoinfo = YoutubeParce(url)
+  RabbitMQSent(videoinfo, queue_info)
+
+
+
 def callback(ch, method, properties, body):
-    data = json.loads(body)
-    print("url: {}".format(data['url']))
-    print("email: {}".format(data['email']))
-    print('Isfile: {}'.format(data['IsFile']))
-    print('format: {}'.format(data['format']))
-    print('quality: {}'.format(data['quality']))
-
-
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+  data = json.loads(body)
+  ReturnVideoInfo(data['url'])
+ 
+  ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='online_converter_queue', on_message_callback=callback)
